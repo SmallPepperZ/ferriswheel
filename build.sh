f0a8f8fe7792eb50a212bed1c5e33fbb08d3e4f1
valkyrie_pilot <valk@randomairborne.dev>
2021-08-07 11:31:22 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 cd /home/gxhut/ferriswheel/
 git fetch
 git reset --hard origin/main
+rm -rf dist/
 python3 -m build
 twine upload --config-file pypi-credentials.txt dist/*
 chown -R gxhut /home/gxhut/ferriswheel
