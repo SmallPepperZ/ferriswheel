aac9f1708a618330b047b60674706441f02768c3
valkyrie_pilot <valk@randomairborne.dev>
2021-08-07 10:30:26 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='ferriswheel',
+      version='0.1',
+      description='https://ferris.chat API library',
+      url='http://github.com/randomairborne/ferriswheel',
+      author='valkyrie_pilot',
+      author_email='valk@randomairborne.dev',
+      license='LGPL',
+      packages=['ferriswheel'],
+      install_requires=[
+          'emoji',
+      ],
+      zip_safe=False)
