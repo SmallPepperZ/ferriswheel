b9ed228bcb72d31576268225f428b3d5cff0aa00
valkyrie_pilot <valk@randomairborne.dev>
2021-08-07 11:06:24 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(name='ferriswheel',
       version='0.1',
       description='https://ferris.chat API library',
       url='http://github.com/randomairborne/ferriswheel',
       author='valkyrie_pilot',
       author_email='valk@randomairborne.dev',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       license='LGPL',
       install_requires=[
           'emoji',
