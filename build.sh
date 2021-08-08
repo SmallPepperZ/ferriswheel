a8f40e668bb42447c674ba31821eb470f1d95a4d
valkyrie_pilot <valk@randomairborne.dev>
2021-08-07 19:14:50 -0500

    improve build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+su gxhut
 cd /home/gxhut/ferriswheel/
 git fetch
 git reset --hard origin/main
@@ -7,3 +8,4 @@ python3 -m build
 twine upload --config-file pypi-credentials.txt dist/*
 chown -R gxhut /home/gxhut/ferriswheel
 chmod +x build.sh
+pip install --upgrade ferriswheel
